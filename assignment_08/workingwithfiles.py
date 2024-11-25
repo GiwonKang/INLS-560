7f3ba7e1f60f7283952a00e68c6ae334ea2b008b
Giwon Kang <gwkang@unc.edu>
2024-11-25 17:25:27 -0500

    updated file name after testing a and b

--- assignment_08/workingwithfiles.py
+++ assignment_08/workingwithfiles.py
@@ -15,9 +15,9 @@ while True:
     if file_variable == 'x':
         sys.exit() # this requires
     elif file_variable == 'a':
-        file_variable = '60s-music file'
+        file_variable = '60s-music.csv'
     elif file_variable == 'b':
-        file_variable = 'athletes.cvs'
+        file_variable = 'athletes.csv'
     else:
         print('Invalid option. Please select a,v, or x.')
         continue
@@ -26,4 +26,42 @@ while True:
     search_variable = search_variable.title() # Make it so that the user can enter lower-case term.
 
 
-    # go to 02_search_for term.
+    # go to 02_search_for term. to continue .....
+    def find(file_variable, search_variable):
+        with open(file_variable, 'r') as file:
+            content = file.read()
+    # now check to the file is in the memory buffer as content
+
+    # Next check to see if the search_variable is in the content buffer:
+            if search_variable in content:
+
+            # if the file print that it is in the file AND
+            # if user wants to see the entries for the term
+                print(f'Your search term {search_variable} exist in the {file_variable} file:')
+                see_entries = input('Would you like to see the entries? (y or n)?')
+
+                # if y then run this code to output all the entreis:
+                if see_entries.lower() == 'y':
+                    print(f'Here are all of the entries with the term {search_variable}:')
+                    with open(file_variable, 'r') as file:
+                        for line in file:
+                            if search_variable in line: #
+                                print(line.strip())
+                
+                
+                
+                # if N lowercase (user does not want to ) then run this code:
+                elif see_entries.lower() == 'n':
+                    print('Goodbye')
+                    sys.exit()
+
+                else:
+                    print(f"Invalid option. Please entry y or n.")
+                
+            
+            # if it is not, print that it is there
+            else:
+                print(f'{search_variable} does not exist in {file_variable}')
+
+    # call the function
+    find(file_variable, search_variable)
